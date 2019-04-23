69ae99686e5ff8130bdcebb3f9c7c2590ecbe7f5
Emilia Ziemichód <emifrompoland@gmail.com>
2019-04-23 01:42:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+class CashRegister
+  attr_accessor :discount, :item, :last_transaction, :total
 
+  def initialize(discount=0)
+    @item=[]
+    @discount=discount
+    @total=0
+  end
+
+  def add_item(price, name, quantity=1)
+    if quantity > 1
+      i=0
+      while i < quantity
+        @item << name
+        i+=1
+      end
+    else
+      @item<<name
+    end
+    @total+=price*quantity
+    @last_transaction=@total
+    @total
+  end
+
+  def apply_discount
+    if discount==0
+      return"There is no discount to apply."
+    end
+    @total-=@total*@discount/100
+    return"After the discount, the total comes to #{@total}."
+  end
+    
+    
+  end
