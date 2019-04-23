f93f9000960b1bcba805b6b5856f63c205fcb4f0
Emilia Ziemichód <emifrompoland@gmail.com>
2019-04-23 02:09:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,21 +8,19 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 1)
-    #@price = price
-    #@total += price * quantity
-    #if quantity > 1
-    #  counter = 0
-    #  while counter < quantity
-    #    @items << item
-    #    counter += 1
-    #  end
-    #else
-    #  @items << item
-    #end
-    self.total += price * quantity
-    quantity.times { self.items << title }
-    self.last_transaction = [item, price, quantity]
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
   end
+    
 
   def apply_discount
     if @discount > 0
